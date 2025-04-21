d31c145301f58e622fd052e9572c0a8e342f91fb
Maria Castro <mcastrod08@gmail.com>
2025-04-21 19:15:44 +0200

    hero section

---
+++ components/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='z-0 min-h-screen bg-black'>dddddd<button>dsfsdfdsf</button></div>
+  )
+}
+
+export default About
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import About from '../components/About'
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-hidden">
       <Hero />
-      <section className='relative z-0 min-h-screen bg-black'> HOLAAAAAAA</section>
+      <About id="watch-trailer" />
     </main>
   )
 }
