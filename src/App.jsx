c661e26a1b24f06b8d4dc98c7ac5a71b66630456
Maria Castro <mcastrod08@gmail.com>
2025-04-23 19:50:58 +0200

    features section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import About from '../components/About'
 import Navbar from '../components/Navbar'
+import Features from '../components/Features'
 
 const App = () => {
   return (
@@ -9,7 +10,8 @@ const App = () => {
       w-screen overflow-hidden">
       <Navbar/>
       <Hero />
-      <About id="watch-trailer" />
+      <About />
+      <Features />
     </main>
   )
 }
